42a5ae24d291dd1b4ba810d820e06352a868b9c8
pRicard0 <pauloricardo.jesus2011@gmail.com>
2023-02-23 20:03:13 -0300

    Basic JS finished

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+function convertPokemonToHTML(pokemon) {
+    return `
+        <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="details">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+        </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+PokeAPI.getPokemons().then((pokemons) => {
+    pokemonList.innerHTML = pokemons.map(convertPokemonToHTML).join('')
+})
---
+++ assets/js/poke-api.js
@@ -0,0 +1,11 @@
+
+
+const PokeAPI = {}
+
+PokeAPI.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .catch((error) => console.log(error))
+}
